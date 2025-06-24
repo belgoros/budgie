f739f4bada9854aa6f6b0e71e9acebe99db81f27
belgoros <s.cambour@gmail.com>
2025-06-24 14:44:33 +0200

    Install nanoid

--- mix.exs
+++ mix.exs
@@ -68,7 +68,8 @@ defmodule Budgie.MixProject do
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
       {:ex_machina, "~> 2.8", only: :test},
-      {:excoveralls, "~> 0.18", only: :test}
+      {:excoveralls, "~> 0.18", only: :test},
+      {:nanoid, "~> 2.1.0"}
     ]
   end
 
